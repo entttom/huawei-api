17b894f357fda42f48fda03171368e754f39cec2
entttom <entner.tom@gmail.com>
2024-01-02 20:55:31 +0100

    Update demo.php

--- demo.php
+++ demo.php
@@ -6,10 +6,10 @@ require_once 'vendor/autoload.php';
 $router = new HSPDev\HuaweiApi\Router();
 
 //If specified without http or https, assumes http://
-$router->setAddress('192.168.8.1');
+$router->setAddress('192.168.0.1');
 
 //Username and password.
 //Username is always admin as far as I can tell.
-$router->login('admin', 'your-password');
+$router->login('admin', 'admin');
 
 var_dump($router->getLedStatus());
